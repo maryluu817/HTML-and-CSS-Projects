c7c3820be545e5ee27c7d9a225d8b2d576d69af4
maryluu817 <109318116+maryluu817@users.noreply.github.com>
2022-09-19 15:13:45 -0400

    updated with css folder

---
+++ Basic_HTML_and_CSS/New Folder/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, World!';
+    console.log(greeting);
+
+var greeting22 = 'Welcome to Node.js';
+console.log(greeting2);
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentence1);
